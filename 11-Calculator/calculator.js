bb51fd56d00f20ce36af503315835cf66f5811fb
GeT_LeFt <913667252hcx@gmail.com>
2022-06-13 10:07:27 +0800

    1

--- 11-Calculator/calculator.js
+++ 11-Calculator/calculator.js
@@ -13,7 +13,18 @@ app.post("/", function (req, res) {
   var num1 = Number(req.body.n1);
   var num2 = Number(req.body.n2);
   var result = num1 + num2;
-  res.send(result);
+  res.send("111" + result);
+});
+
+app.get("/bmiCalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function (req, res) {
+  var weight = Number(req.body.w1);
+  var height = Number(req.body.h2);
+  var result = weight + height;
+  res.send("Your BMI is " + result);
 });
 
 app.listen(3000, function () {
